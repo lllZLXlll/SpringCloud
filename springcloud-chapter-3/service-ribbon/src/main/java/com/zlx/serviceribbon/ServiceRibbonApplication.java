d926095143ab762033ad61313ded05f5d40cfd28
张立享 <zlx_ooo@126.com>
2018-10-11 15:07:54 +0800

    ribbon中使用断路器

--- springcloud-chapter-3/service-ribbon/src/main/java/com/zlx/serviceribbon/ServiceRibbonApplication.java
+++ springcloud-chapter-3/service-ribbon/src/main/java/com/zlx/serviceribbon/ServiceRibbonApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * 消费者
  */
+
+@EnableHystrix // 开启断路器
 @EnableDiscoveryClient // 开启发现服务
 @SpringBootApplication
 public class ServiceRibbonApplication {
@@ -18,7 +21,6 @@ public class ServiceRibbonApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceRibbonApplication.class, args);
     }
-
 
     @Bean
     @LoadBalanced // 表明这个restTemplate开启负载均衡的功能
