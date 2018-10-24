9e026ff8bb82970cda3807486c503a774e0423c9
张立享 <zlx_ooo@126.com>
2018-10-24 15:56:33 +0800

    fix，config手动刷新还有问题

--- springcloud-chapter-6/config-client/src/main/java/com/zlx/serviceclient/ConfigClientApplication.java
+++ springcloud-chapter-6/config-client/src/main/java/com/zlx/serviceclient/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.zlx.serviceclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope // 开启刷新配置
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
